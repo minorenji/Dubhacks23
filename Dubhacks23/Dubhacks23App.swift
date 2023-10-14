2506c06b807d760564893f64df701de7ce093f83
Sean <ssslim2002@gmail.com>
2023-10-14 14:00:18 -0700

    Initial Commit

---
+++ Dubhacks23/Dubhacks23App.swift
@@ -0,0 +1,17 @@
+//
+//  Dubhacks23App.swift
+//  Dubhacks23
+//
+//  Created by Sean Lim on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Dubhacks23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
